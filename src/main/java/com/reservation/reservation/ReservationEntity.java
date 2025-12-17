03b7d3d8c622f35d56de20ccdbcf69bd6efa6070
fermer123 <fermer_525@mail.ru>
2025-12-18 03:10:06 +0500

    hiber

---
+++ src/main/java/com/reservation/reservation/ReservationEntity.java
@@ -0,0 +1,87 @@
+package com.reservation.reservation;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "resrvations")
+@Entity
+public class ReservationEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "room_id")
+    private Long roomId;
+    @Column(name = "end_date")
+    private LocalDate endDate;
+    @Column(name = "start_date")
+    private LocalDate startDate;
+    private ReservationStatus status;
+
+    public ReservationEntity(Long id, Long userId, Long roomId, LocalDate endDate, LocalDate startDate,
+            ReservationStatus status) {
+        this.id = id;
+        this.userId = userId;
+        this.roomId = roomId;
+        this.endDate = endDate;
+        this.startDate = startDate;
+        this.status = status;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public ReservationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReservationStatus status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
