bb6735e030b73958309cc39f3a9b135f834489f2
fermer123 <fermer_525@mail.ru>
2025-12-26 03:37:17 +0500

    Exception

---
+++ src/main/java/com/reservation/reservation/GlobalExeptionHandler.java
@@ -0,0 +1,36 @@
+package com.reservation.reservation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+public class GlobalExeptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(ReservationsController.class);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericExeption(Exception e) {
+        log.error("Handle exeption", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException e) {
+        log.error("Handle EntityNotFoundException exeption", e);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(exception = {
+            IllegalArgumentException.class,
+            IllegalStateException.class
+    })
+    public ResponseEntity<String> handleBadRequest(Exception e) {
+        log.error("Handle BAD_REQUEST exeption", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
--- src/main/java/com/reservation/reservation/ReservationRepository.java
+++ src/main/java/com/reservation/reservation/ReservationRepository.java
@@ -1,7 +1,12 @@
 package com.reservation.reservation;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
+    @Query("select r FROM ReservationEntity r where r.status = :status")
+    List<ReservationEntity> findAllByStatusIs(ReservationStatus status);
 
 }
